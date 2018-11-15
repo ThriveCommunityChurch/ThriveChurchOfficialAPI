a4fb840441fcbe46d6c6f3d9edb837efefe5319b
Wyatt Baggett <wyattbaggett@yahoo.com>
2018-11-14 21:57:57 -0500

    init of .Net Core 2.1 API

---
+++ API/ThriveChurchOfficialAPI/ThriveChurchOfficialAPI/Controllers/PassagesController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace ThriveChurchOfficialAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PassagesController : ControllerBase
+    {
+        private readonly IOptions<AppSettings> config;
+
+        public PassagesController(IOptions<AppSettings> config)
+        {
+            this.config = EsvApiKey;
+        }
+
+        // GET api/passage
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return  "value1 value2";
+        }
+
+        // GET api/passage/{id}
+        [HttpGet("{id}")]
+        public ActionResult<string> GetPassageForId(int id)
+        {
+            return "value";
+        }
+
+        // GET api/passage/{query}
+        [HttpGet("{id}")]
+        public ActionResult<string> GetPassageForQuery(int id)
+        {
+            return "value";
+        }
+    }
+}
---
+++ API/ThriveChurchOfficialAPI/ThriveChurchOfficialAPI/Startup.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System.Configuration;
+
+namespace ThriveChurchOfficialAPI
+{
+    public class Startup
+    {
+        public readonly string EsvApiKey;
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(env.ContentRootPath)
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; set; }
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddMvc();
+            // Add functionality to inject IOptions<T>
+            services.AddOptions();
+
+            // Add our Config object so it can be injected later
+            services.Configure<AppSettings>(Configuration.GetSection("EsvApiKey"));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
