e77ab4b166484c7e812bca8ca8ef722ef5c30135
Wyatt Baggett <wyattbaggett@yahoo.com>
2018-11-14 22:00:14 -0500

    Only ignore the projects app settings, contributors will need this class

---
+++ API/ThriveChurchOfficialAPI/ThriveChurchOfficialAPI/AppSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace ThriveChurchOfficialAPI
+{
+    public class AppSettings
+    {
+        /// <summary>
+        /// Gets the API Key from ESV Api within the Appsettings.json
+        /// </summary>
+        public string EsvApiKey { get; set; }
+    }
+}
