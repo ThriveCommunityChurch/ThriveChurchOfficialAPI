bbe7f7305b39d5e63561cf1c6fbd708844f2c916
Wyatt Baggett <wyattbaggett@yahoo.com>
2018-11-22 23:45:36 -0500

    Hitting a wall, need to determine if it's worth the efforts of making their API consumable on this end
    Might just want to do whatever's easier to display on the UI of the app which would be a string - as long as it needs to be. Probably without the Footnotes too. Unless I want to parse those out like I have and build a fancy UI for that... TBD

--- API/ThriveChurchOfficialAPI/ThriveChurchOfficialAPI.Core/Passage.cs
+++ API/ThriveChurchOfficialAPI/ThriveChurchOfficialAPI.Core/Passage.cs
@@ -6,11 +6,13 @@ namespace ThriveChurchOfficialAPI
     {
         public Passage()
         {
-
+            IsHeader = false;
         }
 
         public int VerseNumber { get; set; }
 
         public string Verse { get; set; }
+
+        public bool IsHeader { get; set; }
     }
 }
