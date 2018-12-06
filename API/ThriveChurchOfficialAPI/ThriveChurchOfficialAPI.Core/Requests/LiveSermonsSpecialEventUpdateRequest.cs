38756a170522f7b8052dfbad517dbef8b04090ff
Wyatt Baggett <wyattbaggett@yahoo.com>
2018-12-06 04:58:49 -0500

    Setting default for SpecialEventTimes

--- API/ThriveChurchOfficialAPI/ThriveChurchOfficialAPI.Core/Requests/LiveSermonsSpecialEventUpdateRequest.cs
+++ API/ThriveChurchOfficialAPI/ThriveChurchOfficialAPI.Core/Requests/LiveSermonsSpecialEventUpdateRequest.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -10,6 +10,7 @@ namespace ThriveChurchOfficialAPI.Core
         {
             Title = null;
             Slug = null;
+            SpecialEventTimes = null;
         }
 
         /// <summary>
