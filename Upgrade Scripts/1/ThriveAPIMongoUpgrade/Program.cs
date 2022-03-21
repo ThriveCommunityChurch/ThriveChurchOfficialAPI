987fe66c85913f072f06f423e714ce4f077743cd
Wyatt Baggett <wyattbaggett@yahoo.com>
2022-03-20 23:31:36 -0400

    init Upgrade Scripts folder

---
+++ Upgrade Scripts/1/ThriveAPIMongoUpgrade/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ThriveAPIMongoUpgrade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // TODO: :all-the-things:
+        }
+    }
+}
